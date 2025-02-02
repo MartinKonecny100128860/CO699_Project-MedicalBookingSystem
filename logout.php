8073f43dca0b70e902e79ce0345ed7d0c0100ce4
Martin Konecny <100128860@student.burnley.ac.uk>
2025-02-02 13:58:16 +0000

    done comments and indentation on every single file since i am bored in a plane

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-session_start(); // Start session
-session_unset(); // Unset session variables
-session_destroy(); // Destroy the session
-header("Location: login.php"); // Redirect to login page
-exit();
+    session_start(); // Start session
+    session_unset(); // Unset session variables
+    session_destroy(); // Destroy the session
+    header("Location: login.php"); // Redirect to login page
+    exit();
 ?>
